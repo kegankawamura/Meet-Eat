70592a539577e4c00b0f04be8025fc7ba612d214
Tomoya Ogura <tommy8sc@gmail.com>
2016-11-12 04:21:35 -0800

    Add poll page

--- hello.py
+++ hello.py
@@ -1,20 +1,31 @@
 #app.py 
-from flask import Flask, render_template, session, redirect, url_for 
+from flask import Flask, render_template, session, redirect, url_for, request 
 from flask.ext.wtf import Form 
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, RadioField
 from flask.ext.bootstrap import Bootstrap
 from wtforms.validators import Required
 
 from wiki_sentiment import * 
+
+import ipdb
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess'
 
 bootstrap = Bootstrap(app)
 
-class SearchForm(Form):
-    searchterm = StringField('What Wikipedia article would you like to analyze?', validators=[Required()])
+class PollForm(Form):
+    price = RadioField(label="price", description="Price", choices=[('$', '$'), ('$$', '$$'), ('$$$', '$$$')])
+    keywords = RadioField("Type", choices=[('As', 'Asian'), ('Am', 'American'), ('Eu', 'European'), ('Af', 'African')])
+    keyterms = StringField("Keyterms")
     submit = SubmitField('Submit')
+
+@app.route("/user/<unique_id>", methods=['GET', 'POST'])
+def poll(unique_id):
+    form = PollForm()
+    if form.validate_on_submit():
+	return render_template('thanks.html')
+    return render_template('poll.html', form=form)
 
 @app.route("/resume")
 def hello():
@@ -22,7 +33,6 @@ def hello():
 
 @app.route("/", methods=['GET', 'POST'])
 def search():
-    form = SearchForm()
     if form.validate_on_submit():
         session['searchterm'] = form.searchterm.data 
         session['sentiment'], session['titlearticle'], session['wikiurl'] = get_sentiment_from_url(get_one_url_from_wiki_search(session.get('searchterm')))
